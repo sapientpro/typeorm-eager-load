e69b9ba35741601f34d30845decaeff543a496f9
Dmytro Kulyk <dmytro.kulyk@sapient.pro>
2025-10-08 12:27:53 +0300

    feat: export flatRelations and types for enhanced module reusability

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { eagerLoad } from './eager-load'
 export { setEagerDataSource, runWithDataSource } from './current-data-source'
+export { flatRelations } from './flat-relation'
+export * from './types';
